ca03d44d1b455de0d385c4e39e26661bc1e71658
patientplatypus <pweyand@gmail.com>
2017-05-09 00:38:37 -0500

    end Monday

--- front/src/components/MyTravels.js
+++ front/src/components/MyTravels.js
@@ -25,18 +25,25 @@ class MyTravels extends Component {
           <div>
             <Parallax strength={300}>
               <Background>
-                <img src="http://www.fillmurray.com/400/300"/>
+                <img src="http://68.media.tumblr.com/87a054d67ec0533fb006d398bf00d650/tumblr_o87wj9YwfJ1rw1i3co5_1280.jpg"/>
                 <div style={{
-                   width: "auto",
-                   height: 300,
+                   width: "100%",
+                   position: "absolute",
+                   height: "auto",
                    backgroundColor: '#450093'
                   }}></div>
-                <img src="http://www.fillmurray.com/500/300"/>
+                <img src="http://68.media.tumblr.com/87a054d67ec0533fb006d398bf00d650/tumblr_o87wj9YwfJ1rw1i3co5_1280.jpg"/>
               </Background>
               <h1>something else</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor et massa quis placerat. Morbi gravida dolor in rutrum suscipit. Cras vehicula sed turpis non iaculis. Sed et libero sagittis, rhoncus augue semper, consectetur neque. Curabitur aliquam augue in diam feugiat suscipit. Sed faucibus rutrum enim ut ultrices. Integer vel risus cursus, porta mi non, consectetur nisl.</p>
 
               <p>Integer id arcu id orci interdum aliquet. Quisque ac ante ligula. Morbi dapibus ipsum tincidunt metus ornare, at pellentesque sem finibus. Sed nibh elit, lacinia vel tellus in, convallis sodales libero. Etiam aliquam metus risus, vitae tempor justo posuere vel. Nulla nibh metus, condimentum non nisi sit amet, tristique aliquam nulla. In mattis arcu venenatis risus hendrerit consectetur.</p>
+
+              <p>Nulla id erat a odio pellentesque molestie id lacinia ipsum. Quisque a est risus. Donec efficitur odio at feugiat rutrum. Duis at nunc sed nisl vestibulum luctus malesuada ultrices arcu. Praesent massa massa, pellentesque quis blandit vitae, sagittis ac arcu. Sed pretium, ex eu fringilla molestie, tellus elit varius metus, nec gravida purus lectus sit amet urna. Suspendisse vitae scelerisque nisi. Phasellus nibh ligula, aliquam a tempus id, varius non ipsum. Morbi in orci bibendum, condimentum ex auctor, vulputate quam. Proin at velit vel orci aliquam semper. Duis nec tortor vel augue convallis suscipit. Praesent interdum tincidunt ante sit amet commodo.</p>
+
+              <p>Etiam pretium enim vel placerat efficitur. Vivamus aliquam et ante a tincidunt. Maecenas vel orci magna. Phasellus molestie dolor eu purus vulputate, ultrices laoreet purus malesuada. Donec non nisi urna. Curabitur dictum, nunc non hendrerit ornare, urna libero hendrerit lorem, non condimentum ligula elit non enim. Maecenas blandit purus a malesuada fermentum. In ullamcorper ex pharetra purus ullamcorper, et porttitor sapien volutpat. Curabitur ornare orci sed metus dapibus, vitae condimentum neque consectetur. Nullam finibus sem id dui molestie volutpat. Donec ultrices ante vitae hendrerit ultricies. Suspendisse blandit tortor sed ex rutrum suscipit. Praesent vehicula vel lacus et viverra. Vestibulum mollis mauris scelerisque velit sollicitudin, ac dapibus velit dapibus.</p>
+
+              <p>Duis suscipit turpis eu arcu faucibus, in rutrum magna aliquam. Aliquam hendrerit ullamcorper massa eu lacinia. Donec eu placerat leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et congue dolor, ut malesuada augue. Mauris nec libero erat. Nam cursus ipsum eu mi porta eleifend. Praesent quis tempus lectus.</p>
             </Parallax>
           </div>
         )
@@ -54,11 +61,6 @@ class MyTravels extends Component {
 
         <TheContainer/>
 
-        <p>Nulla id erat a odio pellentesque molestie id lacinia ipsum. Quisque a est risus. Donec efficitur odio at feugiat rutrum. Duis at nunc sed nisl vestibulum luctus malesuada ultrices arcu. Praesent massa massa, pellentesque quis blandit vitae, sagittis ac arcu. Sed pretium, ex eu fringilla molestie, tellus elit varius metus, nec gravida purus lectus sit amet urna. Suspendisse vitae scelerisque nisi. Phasellus nibh ligula, aliquam a tempus id, varius non ipsum. Morbi in orci bibendum, condimentum ex auctor, vulputate quam. Proin at velit vel orci aliquam semper. Duis nec tortor vel augue convallis suscipit. Praesent interdum tincidunt ante sit amet commodo.</p>
-
-        <p>Etiam pretium enim vel placerat efficitur. Vivamus aliquam et ante a tincidunt. Maecenas vel orci magna. Phasellus molestie dolor eu purus vulputate, ultrices laoreet purus malesuada. Donec non nisi urna. Curabitur dictum, nunc non hendrerit ornare, urna libero hendrerit lorem, non condimentum ligula elit non enim. Maecenas blandit purus a malesuada fermentum. In ullamcorper ex pharetra purus ullamcorper, et porttitor sapien volutpat. Curabitur ornare orci sed metus dapibus, vitae condimentum neque consectetur. Nullam finibus sem id dui molestie volutpat. Donec ultrices ante vitae hendrerit ultricies. Suspendisse blandit tortor sed ex rutrum suscipit. Praesent vehicula vel lacus et viverra. Vestibulum mollis mauris scelerisque velit sollicitudin, ac dapibus velit dapibus.</p>
-
-        <p>Duis suscipit turpis eu arcu faucibus, in rutrum magna aliquam. Aliquam hendrerit ullamcorper massa eu lacinia. Donec eu placerat leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et congue dolor, ut malesuada augue. Mauris nec libero erat. Nam cursus ipsum eu mi porta eleifend. Praesent quis tempus lectus.</p>
 
         <WorldMap/>
       </div>
